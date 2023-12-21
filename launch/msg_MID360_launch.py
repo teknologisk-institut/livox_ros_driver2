357cdc573f53c7aaf5abb0352be2664d62e75c1b
Lars Glud <lag@teknologisk.dk>
2023-12-21 06:48:18 +0000

    Make path to config setable at launch, as well as pointcloud type.

--- launch/msg_MID360_launch.py
+++ launch/msg_MID360_launch.py
@@ -3,9 +3,11 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
 import launch
+from launch.actions import DeclareLaunchArgument, LogInfo
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+from launch_ros.substitutions import FindPackageShare
 
 ################### user configure parameters for ros2 start ###################
-xfer_format   = 1    # 0-Pointcloud2(PointXYZRTL), 1-customized pointcloud format
 multi_topic   = 0    # 0-All LiDARs share the same topic, 1-One LiDAR one topic
 data_src      = 0    # 0-lidar, others-Invalid data src
 publish_freq  = 10.0 # freqency of publish, 5.0, 10.0, 20.0, 50.0, etc.
@@ -14,25 +16,39 @@ frame_id      = 'livox_frame'
 lvx_file_path = '/home/livox/livox_test.lvx'
 cmdline_bd_code = 'livox0000000001'
 
-cur_path = os.path.split(os.path.realpath(__file__))[0] + '/'
-cur_config_path = cur_path + '../config'
-user_config_path = os.path.join(cur_config_path, 'MID360_config.json')
+user_config_path_default = PathJoinSubstitution([
+                    FindPackageShare('livox_ros_driver2'),
+                    "config",
+                    "MID360_config.json"
+                ])
 ################### user configure parameters for ros2 end #####################
 
-livox_ros2_params = [
-    {"xfer_format": xfer_format},
-    {"multi_topic": multi_topic},
-    {"data_src": data_src},
-    {"publish_freq": publish_freq},
-    {"output_data_type": output_type},
-    {"frame_id": frame_id},
-    {"lvx_file_path": lvx_file_path},
-    {"user_config_path": user_config_path},
-    {"cmdline_input_bd_code": cmdline_bd_code}
-]
-
-
 def generate_launch_description():
+    
+    user_config_path_arg = DeclareLaunchArgument(
+              'user_config_path',
+              default_value=user_config_path_default,
+              description="Path to json config file."
+    )
+
+    xfer_format_arg = DeclareLaunchArgument(
+              'xfer_format',
+              default_value='0',
+              description="0-Pointcloud2(PointXYZRTL), 1-customized pointcloud format"
+    )
+
+    livox_ros2_params = [
+      {"xfer_format": LaunchConfiguration('xfer_format')},
+      {"multi_topic": multi_topic},
+      {"data_src": data_src},
+      {"publish_freq": publish_freq},
+      {"output_data_type": output_type},
+      {"frame_id": frame_id},
+      {"lvx_file_path": lvx_file_path},
+      {"user_config_path": LaunchConfiguration('user_config_path') },
+      {"cmdline_input_bd_code": cmdline_bd_code}
+    ]
+
     livox_driver = Node(
         package='livox_ros_driver2',
         executable='livox_ros_driver2_node',
@@ -41,14 +57,4 @@ def generate_launch_description():
         parameters=livox_ros2_params
         )
 
-    return LaunchDescription([
-        livox_driver,
-        # launch.actions.RegisterEventHandler(
-        #     event_handler=launch.event_handlers.OnProcessExit(
-        #         target_action=livox_rviz,
-        #         on_exit=[
-        #             launch.actions.EmitEvent(event=launch.events.Shutdown()),
-        #         ]
-        #     )
-        # )
-    ])
+    return LaunchDescription([user_config_path_arg, xfer_format_arg, livox_driver])
